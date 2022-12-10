26a234e36245d0e0124607e4c4f0a1438a20e4ef
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-10 15:32:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int n;
+    cin>> n;
+    float array[n];
+    for (int i=0;i<n;i++){
+        cin>> array[i];
+    }
+    cout << max(array,n) ;
 
-int main(){
-  return 0;
+
+    return 0;
 }
