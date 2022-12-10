9fab9eb854df182b9afa3063163757535c9fd0b8
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-10 15:32:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+#include <iostream>
+using namespace std;
+float max (float a[],int n){
+    float b=0;
+
+    for (int i = 0; i <= n ; i++) {
+        if ( a[i]>b){
+            b=a[i];
+        }
+    }
+    return b;
+}
